6d4e4dcf02bbe450d3620080d84c462aa463be05
Isidro A. López G <me@ialopezg.com>
2020-09-15 09:47:43 -0500

    Added DatabaseException class. Occurs when database config error.

---
+++ src/Database/DatabaseException.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace ialopezg\Libraries\Database;
+
+/**
+ * An Database Exception.
+ *
+ * @package ialopezg\Libraries\Database
+ */
+class DatabaseException extends \ErrorException { }
