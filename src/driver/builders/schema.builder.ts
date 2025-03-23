8179038d37c951f45cdce8ca2981eb1a66fe0061
Isidro Lopez <isidro.lopezg@live.com>
2025-03-23 13:27:46 -0400

    feat: add basic SchemaBuilder abstract class

---
+++ src/driver/builders/schema.builder.ts
@@ -0,0 +1,6 @@
+/**
+ * SchemaBuilder defines methods for interacting with a database schema, including operations
+ * such as adding columns, foreign keys, and creating tables. These operations map to DDL queries
+ * to modify the database schema.
+ */
+export abstract class SchemaBuilder {}
